f6699ae284dfdafb04e2f714d8e0083d54cc0772
Peter Milne <peter.milne@d-tacq.com>
2024-03-12 21:26:14 +0000

    Updated to commit 577cff0ea8a00eb38b831bc6f5235bc04be4009b

---
+++ docs/_static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '577cff0ea8a00eb38b831bc6f5235bc04be4009b',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
